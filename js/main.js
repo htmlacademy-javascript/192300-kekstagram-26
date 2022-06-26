26f7f57809cc168c40bec35b923ed2060777a3ad
anastasyadm <kond-ad@yandex.ru>
2022-06-26 18:17:53 +0300

    module

--- js/main.js
+++ js/main.js
@@ -1,21 +1,2 @@
-<<<<<<< Updated upstream
-/* eslint-disable no-console */
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function checkingMaximumLengthString(checkString, maxLength){
-  if (checkString.length >maxLength){
-    return false;
-  }
-  return true;
-
-}
-
-console.log(checkingMaximumLengthString('fjdmdkfmdkfmdkfm',1));
-console.log(getRandomIntInclusive(1, 300));
-=======
 import './data.js';
->>>>>>> Stashed changes
+
